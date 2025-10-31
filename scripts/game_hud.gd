5b512edd0bca096c5df3c88e5aa450e2256fff6a
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2025-10-31 08:40:11 -0400

    Rewrite hud (with sgq_ui assets)

--- scripts/game_hud.gd
+++ scripts/game_hud.gd
@@ -2,8 +2,12 @@
 class_name GameHUD
 extends Control
 
-@onready var BombsLabel: Label = $BombsAndKeys/Bombs/BombsLabel
-@onready var KeysLabel: Label = $BombsAndKeys/Keys/KeysLabel
+
+const BOMB_ICON_SCENE: PackedScene = preload("res://components/bomb_icon.tscn")
+const KEY_ICON_SCENE: PackedScene = preload("res://components/keys_icon.tscn")
+
+@onready var BombsBox: HBoxContainer = $BombsAndKeys/Bombs
+@onready var KeysBox: HBoxContainer = $BombsAndKeys/Keys
 
 func _ready() -> void:
 	PickupEventBus.picked_up.connect(_update_and_ignore)
@@ -14,6 +18,12 @@ func _update_and_ignore(_ignored: Variant) -> void:
 	_update()
 
 func _update() -> void:
-	BombsLabel.text = str(PickupEventBus.bombs)
-	KeysLabel.text = str(PickupEventBus.keys)
+	for icon: TextureRect in BombsBox.get_children():
+		icon.queue_free()
+	for icon: TextureRect in KeysBox.get_children():
+		icon.queue_free()
 	
+	for b: int in range(PickupEventBus.bombs):
+		BombsBox.add_child(BOMB_ICON_SCENE.instantiate())
+	for k: int in range(PickupEventBus.keys):
+		KeysBox.add_child(KEY_ICON_SCENE.instantiate())
